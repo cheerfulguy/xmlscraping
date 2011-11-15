c08980f7a6ce375688dcc8fec492a3468aee102e
Abhishek <cheerfulguy@gmail.com>
2011-11-15 17:54:18 -0500

    pretyt much good to go expect only one qualifier tag ifixing that

--- parse.py
+++ parse.py
@@ -6,28 +6,36 @@ def tasks(xml):
     node = doc.documentElement
     records = doc.getElementsByTagName("MedlineCitation")
     MeSHs = []
-    task1 = open("task1.txt","w")
-    task2 = open()
+    task1 = open("task1.csv","w")
+    task2 = open("task2.csvopen","w")
     
+    task1.write("PMID, YEAR\n")
+    task2.write("PMID,DESCRIPTORMAJOR,DESCRIPTORNAME,QUALIFIERNAME,QUALIFIERMAJOR,YEAR\n")
+
     # task1
     for record in records:
-        pmid = record.getElementsByTagName("PMID")[0].firstChild.data
-        year = record.getElementsByTagName("Year")[0].firstChild.data
+        pmid = record.getElementsByTagName("PMID")[0].firstChild.data.strip()
+        year = record.getElementsByTagName("Year")[0].firstChild.data.strip()
         #print pmid, year
+        task1.write(pmid + ", " + year + "\n")
         meshes = record.getElementsByTagName("MeshHeading")
     
-    for mesh in meshes:
+        for mesh in meshes:
         
-        if(len(mesh.getElementsByTagName("DescriptorName"))!=0):
-            descriptor = mesh.getElementsByTagName("DescriptorName")[0]
-            descriptorName =  descriptor.firstChild.data
-            descriptorMajor = descriptor.getAttribute("MajorTopicYN")
-        
-        if(len(mesh.getElementsByTagName("QualifierName"))!=0):
-            qualifier =  mesh.getElementsByTagName("QualifierName")[0]
-            qualifierName =  qualifier.firstChild.data
-            qualifierMajor = qualifier.getAttribute("MajorTopicYN")
-        
+            if(len(mesh.getElementsByTagName("DescriptorName"))!=0):
+                descriptor = mesh.getElementsByTagName("DescriptorName")[0]
+                descriptorName =  descriptor.firstChild.data
+                descriptorMajor = descriptor.getAttribute("MajorTopicYN")
+            
+            if(len(mesh.getElementsByTagName("QualifierName"))!=0):
+                qualifier =  mesh.getElementsByTagName("QualifierName")[0]
+                qualifierName =  qualifier.firstChild.data
+                qualifierMajor = qualifier.getAttribute("MajorTopicYN")
+            else:
+                qualifierName = "."
+                qualifierMajor = "."
+
+            task2.write(pmid +","+descriptorMajor + "," + descriptorName + "," + qualifierName + "," + qualifierMajor + "," + year + "\n")
  
 tasks('data.xml')
 #from BeautifulSoup import BeautifulStoneSoup
